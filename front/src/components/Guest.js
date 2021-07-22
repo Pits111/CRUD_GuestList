a6bb96d1eb96f8927aefebb6c4507b0962ffeacd
Pits111 <isabel.mehlmann@gmail.com>
2021-07-22 16:56:13 +0200

    adding style to header

--- front/src/components/Guest.js
+++ front/src/components/Guest.js
@@ -2,15 +2,17 @@ import "./List.css"
 
 const Guest = ({ name, age, id, deleteGuests }) => {
   return (
-    <div>
-      <div classname="guest">
-        <p>
-          {name} <bn/>
-          ({age})
-        </p>
+    <div className="name-age-wrapper">
+        <div className="name-container">
+          <span>{name}</span>
+        </div>
+        <div className="age-container">
+          <span>{age}</span>
+        </div>
+        
         <button onClick = {() => deleteGuests(id)} className="delete-button">Delete</button>
-      </div>
-      </div>
+        <button onClick = {() => deleteGuests(id)} className="edit-button">Edit</button>
+    </div>
   );
 };
 
