b6cc318af19e6bf8164fcc51c6f53208ded4d4c9
Pits111 <isabel.mehlmann@gmail.com>
2021-07-22 09:54:47 +0200

    adding routes

--- back/index.js
+++ back/index.js
@@ -1,5 +1,11 @@
+require('dotenv').config();
+
 const express = require("express")
 const app = express()
+const guestsRoute = require("./routes/guests-route");
 
-app.listen(5000, () => console.log("server running on port 5000"))
+app.use(express.json());
+app.use("/guests", guestsRoute)
+
+app.listen(5000, () => console.log("Server is running on port 5000"))
 
---
+++ back/routes/guests-route.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const router = express.Router();
+const connection = require("../db/config");
+
+router.get("/", (req, res) => {
+  connection.query("SELECT * FROM guest", (err, results) => {
+    if (err) {
+      res.send("Error retrieving guest from the database");
+    } else {
+      res.send(results);
+    }
+  });
+});
+
+router.post("/", (req, res) => {
+  const { name, age } = req.body;
+  connection.query(
+    "INSERT INTO guest (name, age) VALUES (?, ?)",
+    [name, age],
+    (err, results) => {
+      if (err) {
+        res.send("Error adding the new guest to your list");
+      } else {
+        res.json(req.body);
+      }
+    }
+  );
+});
+
+router.put("/:id", (req, res) => {
+  connection.query(
+    "UPDATE guest SET name=? WHERE id=?",
+    [req.body.name, req.params.id],
+    (err, results) => {
+      if (err) {
+        res.send("Error trying to edit the guest");
+      } else {
+        res.send("Guest was edited");
+      }
+    }
+  );
+});
+
+router.delete("/:id", (req, res) => {
+  connection.query(
+    "DELETE FROM guest WHERE id=?",
+    [req.params.id],
+    (err, results) => {
+      if (err) {
+        res.send("Error deleting guest from the list");
+      } else {
+        res.send("Guest deleted correctly from the list");
+      }
+    }
+  );
+});
+
+module.exports = router;
