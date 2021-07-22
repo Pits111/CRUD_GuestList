4bfc11483ec0426ff07f15f48ba9fb6e49e9888f
Pits111 <isabel.mehlmann@gmail.com>
2021-07-22 16:06:26 +0200

    changes on proxy

--- back/index.js
+++ back/index.js
@@ -7,7 +7,9 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
-app.use("/guests", guestsRoute)
+app.use("/guests", guestsRoute);
+
+app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
 
 // app.get("/", (req, res) => res.send("Hello from /"))
 
@@ -22,5 +24,4 @@ app.use("/guests", guestsRoute)
 
 // })
 
-app.listen(PORT, () => console.log(`Server is running on ${PORT}`))
 
