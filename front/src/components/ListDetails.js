d522393f7016c79371f7f9e5bbc78c3d85ab9cf5
Pits111 <isabel.mehlmann@gmail.com>
2021-07-22 11:14:20 +0200

    changes on List and ListDetails

--- front/src/components/ListDetails.js
+++ front/src/components/ListDetails.js
@@ -1,15 +1,15 @@
 import Guest from "./Guest";
 
-const Table2 = ({ data }) => {
+const ListDetails = ({ data }) => {
   return (
     <div>
       
       {data.map((guest) => (
-        <Item key={data.id} name={guest.name} quantity={guest.age}/>
+        <Guest key={data.id} name={guest.name} quantity={guest.age}/>
       ))}
       <div className="horizontal-line"></div>
     </div>
   );
 };
 
-export default Table2;
+export default ListDetails;
